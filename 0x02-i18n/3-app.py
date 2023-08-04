04e78138ee5a048ae67370945509a82ba7129a22
leroysb <leroybuliro@gmail.com>
2023-08-04 12:59:25 +0300

    bug fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -19,6 +19,7 @@ class Config(object):
 
 
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.locale
@@ -28,10 +29,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel = Babel(app)
 # babel.init_app(app, locale_selector=get_locale)
-
-
 @app.route('/')
 def get_index() -> str:
     """ GET /
