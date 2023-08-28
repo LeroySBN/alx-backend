9147ed3e0828ee3b21ea94c9736829c9f6c372fa
leroysb <leroybuliro@gmail.com>
2023-08-28 15:48:25 +0300

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,30 @@
+// Node Redis Client
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`edis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    console.log(`Reply: ${reply}`);
+  });
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    console.log(reply);
+  });
+}
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
