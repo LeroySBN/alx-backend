59287597f975332a26a09969b12fd7dc4f298d9e
leroysb <leroybuliro@gmail.com>
2023-08-29 10:40:03 +0300

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,16 @@
+// Create the Job creator
+const queue = require('kue').createQueue();
+
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'Random message',
+};
+
+const job = queue.create('push_notification_code', jobData).save((err) => {
+  if (err) console.log('Notification job failed');
+  else console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
