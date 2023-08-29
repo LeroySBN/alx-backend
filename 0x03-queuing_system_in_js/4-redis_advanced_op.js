b9dfced6e20d169254ae8c6f9f55789208324dae
leroysb <leroybuliro@gmail.com>
2023-08-29 22:25:20 +0300

    Node Redis client and advanced operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -11,47 +11,12 @@ client.on('error', (err) => {
 });
 
 // Store a hash value using hset
-client.hset(
-  'HolbertonSchools',
-  'Portland',
-  50,
-  redis.print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'Seattle',
-  80,
-  redis.print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'New York',
-  20,
-  redis.print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'Bogota',
-  20,
-  redis.print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'Cali',
-  40,
-  redis.print
-);
-
-client.hset(
-  'HolbertonSchools',
-  'Paris',
-  2,
-  redis.print
-);
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
 
 // Display the hash value using hgetall
 client.hgetall('HolbertonSchools', (err, result) => {
