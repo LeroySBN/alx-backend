a91fa6681993c9a0c10dc5d49c5682db1da2f668
leroysb <leroybuliro@gmail.com>
2023-08-29 10:52:54 +0300

    Create the Job processor

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,11 @@
+// Create the Job processor
+const kue = require('kue');
+const queue = kue.createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+});
