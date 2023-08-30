98be00cb469c37b46526ddbd9605fea99a10ca82
leroysb <leroybuliro@gmail.com>
2023-08-30 08:17:13 +0300

    Writing the test for job creation

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -21,29 +21,5 @@ describe('createPushNotificationsJobs', () => {
   it('should throw an error if jobs is not an array', () => {
     expect(() => createPushNotificationsJobs('not an array', queue)).to.throw('Jobs is not an array');
   });
-
-  it('should add jobs to the queue', () => {
-    const jobs = [
-      { message: 'Job 1', phoneNumber: '1234567890' },
-      { message: 'Job 2', phoneNumber: '9876543210' },
-    ];
-
-    createPushNotificationsJobs(jobs, queue);
-
-    expect(queue.testMode.jobs.length).to.equal(jobs.length);
-  });
-
-  it('should create two new jobs to the queue', () => {
-    const jobs = [
-      { message: 'Job 1', phoneNumber: '1234567890' },
-      { message: 'Job 2', phoneNumber: '9876543210' },
-    ];
-
-    createPushNotificationsJobs(jobs, queue);
-
-    expect(queue.testMode.jobs.map((job) => job.type)).to.have.members(['push_notification_code_3']);
-  });
-
-  
 });
 
