8f40d148d9cf5e21563e2e86b058c2e43a31757e
leroysb <leroybuliro@gmail.com>
2023-08-30 08:30:05 +0300

    Writing the job creation function

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,23 +1,25 @@
 // Writing the job creation function
 const createPushNotificationsJobs = (jobs, queue) => {
   if (!Array.isArray(jobs)) throw Error('Jobs is not an array');
-  jobs.forEach((job) => {
-    const jobQueue = queue.create('push_notification_code_3', job).save((err) => {
-      if (err) console.log('Notification job failed');
-      else console.log(`Notification job created: ${jobQueue.id}`);
-    });
 
-    jobQueue.on('complete', () => {
-      console.log(`Notification job ${jobQueue.id} completed`);
-    });
+  jobs.forEach(async (job) => {
+    const jobQueue = queue.create('push_notification_code_3', job);
 
-    jobQueue.on('failed', (err) => {
-      console.log(`Notification job ${jobQueue.id} failed: ${err}`);
-    });
+    jobQueue
+      .on('enqueue', () => {
+        console.log(`Notification job created: ${jobQueue.id}`);
+      })
+      .on('complete', () => {
+        console.log(`Notification job ${jobQueue.id} completed`);
+      })
+      .on('failed', (err) => {
+        console.log(`Notification job ${jobQueue.id} failed: ${err}`);
+      })
+      .on('progress', (progress) => {
+        console.log(`Notification job ${jobQueue.id} ${progress}% complete`);
+      });
 
-    jobQueue.on('progress', (progress) => {
-      console.log(`Notification job ${jobQueue.id} ${progress}% complete`);
-    });
+    await jobQueue.save();
   });
 }
 
